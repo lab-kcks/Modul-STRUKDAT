f6fd76224d7aecf2a854077b000ee544b2d50ee5
ramammurshal <rmurshal@gmail.com>
2022-09-09 08:16:11 +0700

    add stack queue code, adjust folder name

---
+++ C/Modul 1 - Struktur Data Linear/queue.c
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+/* Struktur Node */
+typedef struct queueNode_t {
+    int data;
+    struct queueNode_t *next;
+} QueueNode;
+
+/* Struktur ADT Queue */
+typedef struct queue_t {
+    QueueNode   *_front, 
+                *_rear;
+    unsigned _size;
+} Queue;
+
+/* Function prototype */
+void queue_init(Queue *queue);
+bool queue_isEmpty(Queue *queue);
+void queue_push(Queue *queue, int value);
+void queue_pop(Queue *queue);
+int  queue_front(Queue *queue);
+int  queue_size(Queue *queue);
+
+/* Function definition below */
+void queue_init(Queue *queue)
+{
+    queue->_size = 0;
+    queue->_front = NULL;
+    queue->_rear = NULL;
+}
+
+bool queue_isEmpty(Queue *queue) {
+    return (queue->_front == NULL && queue->_rear == NULL);
+}
+
+void queue_push(Queue *queue, int value)
+{
+    QueueNode *newNode = (QueueNode*) malloc(sizeof(QueueNode));
+    if (newNode) {
+        queue->_size++;
+        newNode->data = value;
+        newNode->next = NULL;
+        
+        if (queue_isEmpty(queue))                 
+            queue->_front = queue->_rear = newNode;
+        else {
+            queue->_rear->next = newNode;
+            queue->_rear = newNode;
+        }
+    }
+}
+
+void queue_pop(Queue *queue)
+{
+    if (!queue_isEmpty(queue)) {
+        QueueNode *temp = queue->_front;
+        queue->_front = queue->_front->next;
+        free(temp);
+        
+        if (queue->_front == NULL)
+            queue->_rear = NULL;
+        queue->_size--;
+    }
+}
+
+int queue_front(Queue *queue)
+{
+    if (!queue_isEmpty(queue)) {
+        return (queue->_front->data);
+    }
+    return (int)0;
+}
+
+int queue_size(Queue *queue) {
+    return queue->_size;
+}
+
+int main(int argc, char const *argv[])
+{
+    // Buat objek QueueArray
+    Queue myQueue;
+
+    // PENTING! Jangan lupa diinisialisasi
+    queue_init(&myQueue);
+
+    // Operasi pada queue
+    queue_push(&myQueue, 1);
+    queue_push(&myQueue, 2);
+    queue_push(&myQueue, 3);
+
+    // cetak isi queue
+    while (!queue_isEmpty(&myQueue)) {
+        printf("%d ", queue_front(&myQueue));
+        queue_pop(&myQueue);
+    }
+    puts("");
+    return 0;
+}
---
+++ C/Modul 1 - Struktur Data Linear/stack.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+/* Struktur Node */
+typedef struct stackNode_t {
+    int data;
+    struct stackNode_t *next;
+} StackNode;
+
+/* Struktur ADT Stack */
+typedef struct stack_t {
+    StackNode *_top;
+    unsigned _size;
+} Stack;
+
+/* Function prototype */
+void stack_init(Stack *stack);
+bool stack_isEmpty(Stack *stack);
+void stack_push(Stack *stack, int value);
+void stack_pop(Stack *stack);
+int stack_top(Stack *stack);
+unsigned stack_size(Stack *stack);
+
+/* Function definition below */
+void stack_init(Stack *stack) 
+{
+    stack->_size = 0;
+    stack->_top = NULL;
+}
+
+bool stack_isEmpty(Stack *stack) {
+    return (stack->_top == NULL);
+}
+
+void stack_push(Stack *stack, int value) 
+{
+    StackNode *newNode = (StackNode*) malloc(sizeof(StackNode));
+    if (newNode) {
+        stack->_size++;
+        newNode->data = value;
+        
+        if (stack_isEmpty(stack)) newNode->next = NULL;
+        else newNode->next = stack->_top;
+
+        stack->_top = newNode;
+    }
+}
+
+void stack_pop(Stack *stack) 
+{
+    if (!stack_isEmpty(stack)) {
+        StackNode *temp = stack->_top;
+        stack->_top = stack->_top->next;
+        free(temp);
+        stack->_size--;
+    }
+}
+
+int stack_top(Stack *stack) 
+{
+    if (!stack_isEmpty(stack)) 
+        return stack->_top->data;
+    return 0;
+}
+
+unsigned stack_size(Stack *stack) {
+    return stack->_size;
+}
+
+int main(int argc, char const *argv[])
+{
+    // Buat objek Stack
+    Stack myStack;
+
+    // PENTING! Jangan lupa diinisialisasi
+    stack_init(&myStack);
+
+    // Operasi pada stack
+    stack_push(&myStack, 1);
+    stack_push(&myStack, 2);
+    stack_push(&myStack, 3);
+
+    // cetak isi stack
+    while (!stack_isEmpty(&myStack)) {
+        printf("%d ", stack_top(&myStack));
+        stack_pop(&myStack);
+    }
+    puts("");
+    return 0;
+}
